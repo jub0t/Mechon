57ed5e44e3656e3bf95c1b7948ea007f8b388f65
Jareer <jforeverything2007@gmail.com>
2025-04-12 19:13:42 +0500

    sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
     // ...
-    integrations: [tailwind()],
+    integrations: [tailwind(), icon()],
     // ...
 });
