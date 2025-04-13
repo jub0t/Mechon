5cca98b5486038bea031ab3231950476eb96da63
Jareer <jforeverything2007@gmail.com>
2025-04-13 07:05:45 +0500

    123

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+// tailwind.config.js
 
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        neutral: {
+          10: "hsl(var(--color-neutral-10))",
+          15: "hsl(var(--color-neutral-15))",
+          20: "hsl(var(--color-neutral-20))",
+          85: "hsl(var(--color-neutral-85))",
+          90: "hsl(var(--color-neutral-90))",
+        },
+        primary: {
+          100: "hsl(var(--color-primary-100))",
+        },
+        // Add more color categories if needed
+      },
+    },
+  },
+  plugins: [
+
+  ],
+};
